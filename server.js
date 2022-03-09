d0741427d5c63d65da801a328c15d4b3f0789054
unknown <mbrector@gmail.com>
2022-03-09 14:57:47 -0500

    seed works

--- server.js
+++ server.js
@@ -22,6 +22,20 @@ app.get('/pokemon', (req, res) => {
     })
 }); 
 
+app.get('/pokemon/seed', (req, res)=>{
+    Pokemon.create([
+    {name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur"},
+    {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
+    {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
+    {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander"},
+    {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard"},
+    {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
+    {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"}
+    ], (err, data)=>{
+        res.redirect('/pokemon');
+    })
+});
+
 app.get('/pokemon/new', (req, res) => {
     res.render('New')
 })
