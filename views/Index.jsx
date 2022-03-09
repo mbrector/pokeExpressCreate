ba48b311f806ff3c3229b84c6cd046df265567d0
unknown <mbrector@gmail.com>
2022-03-09 15:36:54 -0500

    node in json

--- views/Index.jsx
+++ views/Index.jsx
@@ -9,7 +9,7 @@ class Index extends React.Component {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Pokemon</title>
 </head>
 <body style = {{backgroundColor: 'aquamarine'}}>
     <h1 style = {{color: 'blueviolet'}}>See All The Pokemon!</h1>
